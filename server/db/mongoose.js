09e4a0db551bc464ba8310cf0adc4df1c51c2b44
Bahtiyar <bahtiyar@Bahtiyars-MacBook-Pro.local>
2018-05-06 18:12:12 +0800

    Modify connect address 4

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const localDb = 'mongodb://localhost:27017/dani';
-const cloudDb = 'mongodb://bahtiyar_a:123456@ds014658.mlab.com:14658/dani';
+const localDb = 'mongodb://127.0.0.1:27017/dani';  // local database
+const cloudDb = 'mongodb://bahtiyar_a:123456@ds014658.mlab.com:14658/dani';  //MongoLab cloud-hosted database
 
 mongoose.connect(localDb||cloudDb);
 
