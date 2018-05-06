61e410e5537f358e745df97eccb9c0e4763820b0
Bahtiyar <bahtiyar@Bahtiyars-MacBook-Pro.local>
2018-05-06 15:48:02 +0800

    only connect to mongo lab cloud database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/dani' || 'mongodb://bahtiyar_a:123456@ds014658.mlab.com:14658/dani');
+mongoose.connect('mongodb://bahtiyar_a:123456@ds014658.mlab.com:14658/dani');
 
 module.exports = {mongoose};
+
+// 'mongodb://localhost:27017/dani'
