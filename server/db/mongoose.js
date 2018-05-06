87883c95a4309f0c6011766d43f1cec625bdb973
Bahtiyar <bahtiyar@Bahtiyars-MacBook-Pro.local>
2018-05-06 11:42:23 +0800

    Add MongoLab cloud-hosted database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/dani');
+mongoose.connect('mongodb://bahtiyar_a:123456@ds014658.mlab.com:14658/dani' || 'mongodb://localhost:27017/dani');
 
 module.exports = {mongoose};
