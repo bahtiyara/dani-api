bdb176151117f24b9b41622276fcf90d4766f50b
Bahtiyar <bahtiyar@Bahtiyars-MacBook-Pro.local>
2018-05-06 17:53:30 +0800

    Modify connect address 3

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,13 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-try {
-    mongoose.connect('mongodb://localhost:27017/dani' || 'mongodb://bahtiyar_a:123456@ds014658.mlab.com:14658/dani');
-} catch (error) {
-    
-}
+const localDb = 'mongodb://localhost:27017/dani';
+const cloudDb = 'mongodb://bahtiyar_a:123456@ds014658.mlab.com:14658/dani';
 
+mongoose.connect(localDb||cloudDb);
 
 module.exports = {mongoose};
-
-// 'mongodb://localhost:27017/dani'
