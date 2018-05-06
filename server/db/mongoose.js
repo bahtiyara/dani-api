d31adca96e81a369b103583308ba32e9cdf2b921
Bahtiyar <bahtiyar@Bahtiyars-MacBook-Pro.local>
2018-05-06 17:37:52 +0800

    Modify connect address 2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/dani' || 'mongodb://bahtiyar_a:123456@ds014658.mlab.com:14658/dani');
+
+try {
+    mongoose.connect('mongodb://localhost:27017/dani' || 'mongodb://bahtiyar_a:123456@ds014658.mlab.com:14658/dani');
+} catch (error) {
+    
+}
+
 
 module.exports = {mongoose};
 
